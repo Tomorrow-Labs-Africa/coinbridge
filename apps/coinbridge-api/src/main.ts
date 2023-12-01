4790895845b271521f7e5db938d234003a4680c9
Jeffrey King'ori <jeffreygithongo@gmail.com>
2023-12-01 12:51:22 +0300

    :wrench: fix/ Set access control headers

--- apps/coinbridge-api/src/main.ts
+++ apps/coinbridge-api/src/main.ts
@@ -15,10 +15,20 @@ const app = express();
 
 const SERVER_BASE_PATH = '/api/v1';
 app.use(express.json())  
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // TODO: Change this to the domain of the frontend app
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.use(`${SERVER_BASE_PATH}/transactions`, TransactionRoutes);
 app.use(`${SERVER_BASE_PATH}/auth`, AuthRoutes);
+
+
 
 const port = process.env.PORT || 3333;
 
