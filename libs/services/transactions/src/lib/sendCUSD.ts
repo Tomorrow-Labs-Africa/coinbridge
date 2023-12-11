8471400587472bb7e3e11ce0bea6f267a7fc6cd1
AlphonceMutebi <60338643+AlphonceMutebi@users.noreply.github.com>
2023-12-11 12:00:30 +0300

    changes to contract call

--- libs/services/transactions/src/lib/sendCUSD.ts
+++ libs/services/transactions/src/lib/sendCUSD.ts
@@ -4,29 +4,40 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const RPC_URL = 'https://forno.celo.org'
-
+const CUSD_CONTRACT = '0x765DE816845861e75A25fCA122bb6898B8B1282a'
 export const sendCUSD = async (
     cusdAmount?:string )=>{
     
         let rpcUrl = RPC_URL
         const pk:string = (process.env.PK as string)
+        console.log('pk: ', pk)
+
+
 
         const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
         const wallet = new ethers.Wallet(pk, provider);
-        const gasPrice  = await (await provider.getFeeData()).gasPrice;
-        const nonce = await provider.getTransactionCount(wallet.getAddress());
+        let abi = ["function transfer(address to, uint256 value)"]
+        const contract = new Contract(CUSD_CONTRACT,abi,wallet);
+        let tx = await contract.transfer('0xb5B00313da3aA8F6463228DBFD6A23740Ca291a1', parseEther('0.1'))
+
+        let receipt = await tx.wait();
+
+        console.log('Receipt is: ', receipt)
+        return receipt
+        // const gasPrice  = await (await provider.getFeeData()).gasPrice;
+        // const nonce = await provider.getTransactionCount(wallet.getAddress());
   
-        // TODO make this dynamic
-        const result = await wallet.sendTransaction({
-            to: '0xb5B00313da3aA8F6463228DBFD6A23740Ca291a1',
-            value: ethers.utils.parseEther('0.1'),
-            gasPrice:  gasPrice!,
-            nonce: nonce,
-        })
+        // // TODO make this dynamic
+        // const result = await wallet.sendTransaction({
+        //     to: '0xb5B00313da3aA8F6463228DBFD6A23740Ca291a1',
+        //     value: ethers.utils.parseEther('0.1'),
+        //     gasPrice:  gasPrice!,
+        //     nonce: nonce,
+        // })
 
-        console.log('result: ', result)
+        // console.log('result: ', result)
 
-        return result
+        // return result
 
 
 }
