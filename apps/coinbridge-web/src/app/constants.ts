d3e777d90e07afd95c842b0e0efe981aa744925f
AlphonceMutebi <60338643+AlphonceMutebi@users.noreply.github.com>
2023-12-11 12:17:23 +0300

    changed base url

--- apps/coinbridge-web/src/app/constants.ts
+++ apps/coinbridge-web/src/app/constants.ts
@@ -1,4 +1,4 @@
 export const CUSD_CONTRACT = '0x765DE816845861e75A25fCA122bb6898B8B1282a'
 export const RPC_URL= 'https://forno.celo.org'
 export const COLLECTION_ESCROW='0xf9436398a70146f58f535c6b93d6845d1afb390b'
-export const BASE_URL= 'https://coinbridge-api.onrender.com/api/v1'
+export const BASE_URL= 'http://localhost:3000/api/v1'
