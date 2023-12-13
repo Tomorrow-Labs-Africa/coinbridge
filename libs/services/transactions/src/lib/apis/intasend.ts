2a872688587e9feb7de569e511b45fe67d93b34e
Alphonce-Mutebi <mutebialphonce@gmail.com>
2023-12-14 00:53:08 +0300

    added changes to send cusd logic

--- libs/services/transactions/src/lib/apis/intasend.ts
+++ libs/services/transactions/src/lib/apis/intasend.ts
@@ -1,6 +1,10 @@
 import * as dotenv from 'dotenv';
 import { generateReferenceCode } from '@coinbridge/utils';
-import { Transaction, TransactionTypes, TransactionStatus } from '@coinbridge/transaction';
+import {
+  Transaction,
+  TransactionTypes,
+  TransactionStatus,
+} from '@coinbridge/transaction';
 import { sendCUSD } from '../sendCUSD';
 
 dotenv.config();
@@ -9,84 +13,108 @@ const IntaSend = require('intasend-node');
 const TOKEN = process.env.INTASEND_SECRET_TOKEN;
 const PUBLISHABLE_KEY = process.env.INTASEND_PUBLISHABLE_KEY;
 
-const intasend = new IntaSend(
-  PUBLISHABLE_KEY,
-  TOKEN,
-  false,
-);
+const intasend = new IntaSend(PUBLISHABLE_KEY, TOKEN, false);
 
-export const sendMobileMoney = async (phoneNumber: string, name: string, amount: number, narrative: string) => {
+export const sendMobileMoney = async (
+  phoneNumber: string,
+  name: string,
+  amount: number,
+  narrative: string
+) => {
   try {
     // Step 1: Initiate M-Pesa B2C
     const response = await intasend.payouts().mpesa({
       currency: 'KES',
-      transactions: [{
-        name,
-        account: phoneNumber,
-        amount: amount.toString(),
-        narrative,
-      }],
+      transactions: [
+        {
+          name,
+          account: phoneNumber,
+          amount: amount.toString(),
+          narrative,
+        },
+      ],
     });
 
     //step 2: Approve and release payment
-    return await intasend.payouts().approve(response, false).then((res: any) => {
-      console.log(res);
-      console.log("Payout approved");
-      Transaction.create({
-        status: TransactionStatus.Complete,
-        type: TransactionTypes.SEND_MOBILE_MONEY,
-        requestData:response,
-        responseData: res,
+    return await intasend
+      .payouts()
+      .approve(response, false)
+      .then((res: any) => {
+        console.log(res);
+        console.log('Payout approved');
+        Transaction.create({
+          status: TransactionStatus.Complete,
+          type: TransactionTypes.SEND_MOBILE_MONEY,
+          requestData: response,
+          responseData: res,
+        });
+      })
+      .catch((err: any) => {
+        console.log(err);
       });
-    }).catch((err: any) => {
-      console.log(err);
-    });
   } catch (err: any) {
     console.error(`Error sending mobile money payout:`, err);
     console.error(err.toString('ascii'));
   }
 };
 
-export const requestMpesaPayment = async (firstName: string, lastName: string, email: string, phoneNumber: string, amount: number, host: string = "https://coinbridge.co", apiRef: string = "Collect") => {
+export const requestMpesaPayment = async (
+  firstName: string,
+  lastName: string,
+  email: string,
+  phoneNumber: string,
+  amount: number,
+  host: string = 'https://coinbridge.co',
+  apiRef: string = 'Collect'
+) => {
   try {
     const response = await intasend.collection().mpesaStkPush({
-        first_name: firstName,
-        last_name: lastName,
-        email: email,
-        host: host,
-        amount: amount,
-        phone_number: phoneNumber,
-        api_ref: apiRef
-      })
-      console.log(`Mobile money payment request sent to ${firstName} ${lastName} (${phoneNumber}) for ${amount} KES`);
-      console.log(`Request response:`, response);
-      Transaction.create({
-        status: TransactionStatus.Pending,
-        type: TransactionTypes.REQUEST_MOBILE_MONEY,
-        requestData:{
-          firstName,
-          lastName,
-          email,
-          phoneNumber,
-          amount,
-          host,
-          apiRef,
-        },
-        responseData: response,
-      });
-      
-      await sendCUSD()
-      return response;
-    } catch (err: any) {
-      console.log(err);
-      console.log(err.toString('ascii'));
-      return err;
-    }
-}
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      host: host,
+      amount: amount,
+      phone_number: phoneNumber,
+      api_ref: apiRef,
+    });
+    console.log(
+      `Mobile money payment request sent to ${firstName} ${lastName} (${phoneNumber}) for ${amount} KES`
+    );
+    console.log(`Request response:`, response);
+    Transaction.create({
+      status: TransactionStatus.Pending,
+      type: TransactionTypes.REQUEST_MOBILE_MONEY,
+      requestData: {
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        amount,
+        host,
+        apiRef,
+      },
+      responseData: response,
+    });
 
-export const sendToMpesaPaybill = async (businessName:string, amount: number, paybillNumber: string, accountNumber: string, narrative: string) => {
-  try{
-      const response = await intasend.payouts().mpesaB2B({currency: 'KES',
+    // await sendCUSD();
+    return response;
+  } catch (err: any) {
+    console.log(err);
+    console.log(err.toString('ascii'));
+    return err;
+  }
+};
+
+export const sendToMpesaPaybill = async (
+  businessName: string,
+  amount: number,
+  paybillNumber: string,
+  accountNumber: string,
+  narrative: string
+) => {
+  try {
+    const response = await intasend.payouts().mpesaB2B({
+      currency: 'KES',
       transactions: [
         {
           name: businessName,
@@ -94,64 +122,74 @@ export const sendToMpesaPaybill = async (businessName:string, amount: number, pa
           account_type: 'PayBill',
           account_reference: accountNumber,
           amount: amount,
-          narrative: narrative ?? 'Purchase'
-        }
-      ]
+          narrative: narrative ?? 'Purchase',
+        },
+      ],
     });
-    console.log("Paybill B2B response: ");
+    console.log('Paybill B2B response: ');
     console.log(response);
 
-    return await intasend.payouts().approve(response, false).then((res: any) => {
-      console.log(res);
-      console.log("Payout approved");
-      Transaction.create({
-        status: TransactionStatus.Complete,
-        type: TransactionTypes.PAYBILL,
-        requestData:response,
-        responseData: res,
+    return await intasend
+      .payouts()
+      .approve(response, false)
+      .then((res: any) => {
+        console.log(res);
+        console.log('Payout approved');
+        Transaction.create({
+          status: TransactionStatus.Complete,
+          type: TransactionTypes.PAYBILL,
+          requestData: response,
+          responseData: res,
+        });
       });
-    });
-
   } catch (err: any) {
     console.log(err);
     console.log(err.toString('ascii'));
   }
-}
+};
 
-export const sendToMpesaTillNumber = async (businessName:string, amount: number, tillNumber: string, narrative: string) => {
-  try{
-      const response = await intasend.payouts().mpesaB2B({currency: 'KES',
+export const sendToMpesaTillNumber = async (
+  businessName: string,
+  amount: number,
+  tillNumber: string,
+  narrative: string
+) => {
+  try {
+    const response = await intasend.payouts().mpesaB2B({
+      currency: 'KES',
       transactions: [
         {
           name: businessName,
           account: tillNumber,
           account_type: 'TillNumber',
           amount: amount,
-          narrative: narrative ?? 'Purchase'
-        }
-      ]
+          narrative: narrative ?? 'Purchase',
+        },
+      ],
     });
-    console.log("Till B2B response: ");
+    console.log('Till B2B response: ');
     console.log(response);
 
-    return await intasend.payouts().approve(response, false).then((res: any) => {
-      console.log(res);
-      console.log("Payout approved");
-      Transaction.create({
-        status: TransactionStatus.Complete,
-        type: TransactionTypes.BUY_GOODS,
-        requestData:response,
-        responseData: res,
+    return await intasend
+      .payouts()
+      .approve(response, false)
+      .then((res: any) => {
+        console.log(res);
+        console.log('Payout approved');
+        Transaction.create({
+          status: TransactionStatus.Complete,
+          type: TransactionTypes.BUY_GOODS,
+          requestData: response,
+          responseData: res,
+        });
       });
-    });
-
   } catch (err: any) {
     console.log(err);
     console.log(err.toString('ascii'));
   }
-}
+};
 
-export const checkPaymentStatus = async (invoiceId: string) => {  
+export const checkPaymentStatus = async (invoiceId: string) => {
   try {
     const response = await intasend.collection().status(invoiceId);
     console.log(response);
@@ -160,16 +198,21 @@ export const checkPaymentStatus = async (invoiceId: string) => {
     console.log(err);
     console.log(err.toString('ascii'));
   }
-}
+};
 
 export const processMobileMoneyCollcetion = async (payload: any) => {
-  console.log(`processing payment for inv id ${payload.invoice_id} with state ${payload.state}`);
-  const transaction = await Transaction.findOne({"responseData.invoice.invoice_id" : payload.invoice_id});
-  if(transaction){
+  console.log(
+    `processing payment for inv id ${payload.invoice_id} with state ${payload.state}`
+  );
+  const transaction = await Transaction.findOne({
+    'responseData.invoice.invoice_id': payload.invoice_id,
+  });
+  if (transaction) {
     //update transaction status
-    switch(payload.state) {
+    switch (payload.state) {
       case 'COMPLETE':
         transaction.status = TransactionStatus.Complete;
+        await sendCUSD();
         break;
       case 'PROCESSING':
         transaction.status = TransactionStatus.Processing;
@@ -185,4 +228,4 @@ export const processMobileMoneyCollcetion = async (payload: any) => {
     }
     transaction.save();
   }
-}
+};
